2fd2c9e649553bd8987069af7a27e7211e2a4de1
kiplah <kiplahvictor27@gmail.com>
2026-04-29 12:39:17 +0300

    changed the scafolded project from flask framework to a django project

--- web/app.py
+++ web/app.py
@@ -1,10 +1,1 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# This file is deprecated. Please refer to views.py and urls.py.
