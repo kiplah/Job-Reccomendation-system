f643b8401a100d066fe6498b1a066813015ad84e
kiplah <kiplahvictor27@gmail.com>
2026-04-29 16:42:59 +0300

    Configure admin panel for accounts and jobs

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Job, JobEmbedding
 
-# Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'location', 'is_active', 'date_posted')
+    search_fields = ('title', 'company', 'location')
+    list_filter = ('is_active',)
+
+admin.site.register(JobEmbedding)
