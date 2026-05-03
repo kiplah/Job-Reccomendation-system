61dec9f6a2f0f362b41d9912bcf68857c7c2513d
kiplah <kiplahvictor27@gmail.com>
2026-05-03 21:25:09 +0300

    updated the landing page to match the proffesional look

--- job_project/urls.py
+++ job_project/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/dashboard/', permanent=False)),
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('alerts/', include('alerts.urls')),
